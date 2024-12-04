0e4da4a3b48f91eeee532cda79456b7cd806248d
OlaShu112 <149779424+OlaShu112@users.noreply.github.com>
2024-12-04 14:06:35 +0000

    Add files via upload

---
+++ resources/views/branch/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('admin.app')
+
+@section('content')
+<div class="content-header-left col-md-9 col-12 mb-2">
+    <div class="row breadcrumbs-top">
+        <div class="col-12">
+            <div class="breadcrumb-wrapper col-12">
+                <h3 class="content-header-title float-left mb-0">Edit Branch</h3>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ url('/') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('branches.index') }}">Branches</a></li>
+                    <li class="breadcrumb-item active">Edit Branch</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section id="dashboard-analytics">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Edit Branch</h4>
+                </div>
+                <div class="card-content">
+                    <div class="card-body">
+                        <form action="{{ route('branches.update', $branch->branch_id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="branch_name">Branch Name</label>
+                                <input type="text" name="branch_name" class="form-control" value="{{ old('branch_name', $branch->branch_name) }}" required>
+                                @error('branch_name')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="location">Location</label>
+                                <input type="text" name="location" class="form-control" value="{{ old('location', $branch->location) }}" required>
+                                @error('location')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="contact_number">Contact Number</label>
+                                <input type="text" name="contact_number" class="form-control" value="{{ old('contact_number', $branch->contact_number) }}" required>
+                                @error('contact_number')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Update Branch</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
